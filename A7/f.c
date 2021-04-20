76c835526660bbed16244722d342e5e013ec6011
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2021-04-21 03:19:39 +0530

    Update f.c

--- A7/f.c
+++ A7/f.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#define N 500005
+#define A 26
+int t[N][A], cnt[N];
+char s[N];
 
+int main() {
+    int Q;
+    for(scanf("%d", &Q); Q--; ) {
+        int n, sz = 1, win = 0, itsover = 0; char prvc;
+        scanf("%d", &n);
+        for(int i = 0, j, p; p = 0, i < n; i++, prvc = s[j-1]) {
+            scanf(" %s", s);
+            itsover |= i && s[0] ^ prvc;
+            if(itsover) continue;
+            for(j = 0; s[j]; j++)
+                p = t[p][s[j]-'a'] = t[p][s[j]-'a']? t[p][s[j]-'a'] : sz++;
+            if(cnt[p]++) itsover = 1;
+            else win ^= 1;
+        }
+        memset(t, 0, sz * A * sizeof **t);
+        memset(cnt, 0, sz * sizeof *cnt);
+        printf("%s ", win? "Alice" : "Bob");
+    }
+}
