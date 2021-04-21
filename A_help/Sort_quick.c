5484154dec348704954edb475e10df18dff01436
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2021-04-21 08:49:16 +0530

    Update Sort_quick.c

--- A_help/Sort_quick.c
+++ A_help/Sort_quick.c
@@ -1,1 +1,13 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int cmpfunc (const void * a, const void * b) {
+   return ( *(int*)a - *(int*)b );
+}
+int main () {
+   int values[] = { 88, 56, 100, 2, 25 };
+   qsort(values, 5, sizeof(int), cmpfunc);
+   for(int n = 0 ; n < 5; n++ ) {   
+      printf("%d ", values[n]);
+   }
+   return(0);
+}
