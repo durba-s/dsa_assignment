fa8fac2400a10508942850bc0f806e74093d1693
Durba Satpathi <70879852+durba-s@users.noreply.github.com>
2021-04-21 08:59:47 +0530

    Update Sort_quick.c

--- A_help/Sort_quick.c
+++ A_help/Sort_quick.c
@@ -11,3 +11,30 @@ int main () {
    }
    return(0);
 }
+
+
+//
+int partition(int arr[],int l,int h){
+    int e=arr[h];
+    int i=l;
+    int j=l;
+    while(j<h){
+        if(e>=arr[j]){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+        }
+        j++;
+    }
+    int temp=arr[h];
+    arr[h]=arr[i];
+    arr[i]=temp;
+    return i;
+}
+void quicksort(int arr[],int l,int h){
+    if(h<=l)    return;
+    int m=partition(arr,l,h);
+    quicksort(arr,l,m-1);
+    quicksort(arr,m+1,h);
+}
